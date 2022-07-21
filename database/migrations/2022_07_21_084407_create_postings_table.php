3e598b697d447c0b70ae96252927bcc1030f7f68
bagashezky <bagashpratama95@gmail.com>
2022-07-21 15:52:51 +0700

    first commit

---
+++ database/migrations/2022_07_21_084407_create_postings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postings', function (Blueprint $table) {
+            $table->bigIncrements('idcaption');
+            $table->string('caption');
+            $table->string('file');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postings');
+    }
+}
